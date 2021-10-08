98ae98dc437b228a2b36a806f92dc8dce04cdb23
Thomas Gaubert <thomas.gaubert49@gmail.com>
2021-10-08 15:46:26 +0200

    App structure

---
+++ app/src/main/java/imt/legrand_gaubert/BookDetails.kt
@@ -0,0 +1,11 @@
+package imt.legrand_gaubert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_details)
+    }
+}
---
+++ app/src/main/java/imt/legrand_gaubert/model/Book.kt
@@ -0,0 +1,4 @@
+package imt.legrand_gaubert.model
+
+class Book {
+}
