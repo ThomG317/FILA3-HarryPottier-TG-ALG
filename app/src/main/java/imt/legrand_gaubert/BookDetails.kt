a62b2c0ec11922932ae4d46986df9c6cc1dd40d7
Thomas Gaubert <thomas.gaubert49@gmail.com>
2021-10-14 09:37:19 +0200

    feat: landscape mode

--- app/src/main/java/imt/legrand_gaubert/BookDetails.kt
+++ app/src/main/java/imt/legrand_gaubert/BookDetails.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.*
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
@@ -23,36 +24,45 @@ class BookDetails : Fragment() {
         val globalVariable: GlobalClass = this.activity?.applicationContext as GlobalClass
         val cart = globalVariable.appShopingCart
         val book = arguments?.getParcelable<Book>("BOOK")
-
-        val button = view?.findViewById<Button>(R.id.details_button)
         val imageView = view?.findViewById<ImageView>(R.id.details_book_cover)
         val titleTextView = view?.findViewById<TextView>(R.id.details_book_title)
         val pricetextView = view?.findViewById<TextView>(R.id.details_price)
         val synopsisTextView = view?.findViewById<TextView>(R.id.details_synopsis)
+        val button = view?.findViewById<Button>(R.id.details_button)
 
+        if (book == null) {
+            imageView?.visibility = GONE
+            titleTextView?.visibility = GONE
+            pricetextView?.text = "Aucun livre séletionné"
+            pricetextView?.textAlignment = TEXT_ALIGNMENT_CENTER
+            pricetextView?.setTextColor(Color.GRAY)
+            synopsisTextView?.visibility = GONE
+            button?.visibility = GONE
+        } else {
 
-        if (cart.find { it.isbn.equals(book?.isbn)} != null) {
-            button?.isEnabled ?: false
-            button?.setTextColor(Color.GRAY)
-            button?.setBackgroundColor(Color.LTGRAY)
-            button?.text = "Déjà ajouté au panier !"
-        }
-
-        button?.setOnClickListener {
-            if (book != null) {
-                cart.add(book!!)
+            if (cart.find { it.isbn.equals(book?.isbn)} != null) {
+                button?.isEnabled ?: false
+                button?.setTextColor(Color.GRAY)
+                button?.setBackgroundColor(Color.LTGRAY)
+                button?.text = "Déjà ajouté au panier !"
             }
-            globalVariable.appShopingCart = cart
-            button?.isEnabled ?: false
-            button?.setTextColor(Color.GRAY)
-            button?.setBackgroundColor(Color.LTGRAY)
-            button?.text = "Déjà ajouté au panier !"
+
+            button?.setOnClickListener {
+                if (book != null) {
+                    cart.add(book!!)
+                }
+                globalVariable.appShopingCart = cart
+                button?.isEnabled ?: false
+                button?.setTextColor(Color.GRAY)
+                button?.setBackgroundColor(Color.LTGRAY)
+                button?.text = "Déjà ajouté au panier !"
+            }
+            Log.i(null, view.toString())
+            Picasso.get().load(book?.cover).into(imageView)
+            titleTextView?.text = book?.title
+            pricetextView?.text = book?.price.toString().plus(" €")
+            synopsisTextView?.text = book?.synopsis?.joinToString("\n")
         }
-        Log.i(null, view.toString())
-        Picasso.get().load(book?.cover).into(imageView)
-        titleTextView?.text = book?.title
-        pricetextView?.text = book?.price.toString().plus(" €")
-        synopsisTextView?.text = book?.synopsis?.joinToString("\n")
 
         return view;
     }
--- app/src/main/java/imt/legrand_gaubert/MainActivity.kt
+++ app/src/main/java/imt/legrand_gaubert/MainActivity.kt
@@ -1,6 +1,8 @@
 package imt.legrand_gaubert
 
 import android.content.Intent
+import android.content.res.Configuration.ORIENTATION_LANDSCAPE
+import android.content.res.Configuration.ORIENTATION_PORTRAIT
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
@@ -14,13 +16,29 @@ import imt.legrand_gaubert.model.BookHolder
 class MainActivity : AppCompatActivity(), BookHolder.Listener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        if (getResources().getConfiguration().orientation == ORIENTATION_PORTRAIT) {
+            setContentView(R.layout.main_activity)
+        } else {
+            setContentView(R.layout.main_activity_land)
+        }
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        supportFragmentManager.beginTransaction()
-            .setCustomAnimations(R.anim.fragment_fade_enter, R.anim.fragment_fade_exit)
-            .replace(R.id.fragment, ListBook(), ListBook::class.java.name)
-            .commit()
+        if (getResources().getConfiguration().orientation == ORIENTATION_PORTRAIT) {
+            supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.fragment_fade_enter, R.anim.fragment_fade_exit)
+                .replace(R.id.fragment, ListBook(), ListBook::class.java.name)
+                .commit()
+        } else {
+            supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.fragment_fade_enter, R.anim.fragment_fade_exit)
+                .replace(R.id.fragment_list, ListBook(), ListBook::class.java.name)
+                .commit()
+
+            supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.fragment_fade_enter, R.anim.fragment_fade_exit)
+                .replace(R.id.fragment_detail, BookDetails(), BookDetails::class.java.name)
+                .commit()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -44,11 +62,19 @@ class MainActivity : AppCompatActivity(), BookHolder.Listener {
         val args =  Bundle()
         args.putParcelable("BOOK", item)
         fragment.arguments = args
-        supportFragmentManager.beginTransaction()
-            .setCustomAnimations(R.anim.fragment_open_enter, R.anim.fragment_close_exit)
-            .replace(R.id.fragment, fragment, BookDetails::class.java.name)
-            .addToBackStack(BookDetails::class.java.name)
-            .commit()
+
+        if (getResources().getConfiguration().orientation == ORIENTATION_PORTRAIT) {
+            supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.fragment_open_enter, R.anim.fragment_close_exit)
+                .replace(R.id.fragment, fragment, BookDetails::class.java.name)
+                .addToBackStack(BookDetails::class.java.name)
+                .commit()
+        } else {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_detail, fragment, BookDetails::class.java.name)
+                .addToBackStack(BookDetails::class.java.name)
+                .commit()
+        }
     }
 
 }
